d654ffde14fc5e4cf451522a995f1230d6e61055
Miguel Sanchez <miguel.sanchez@cirvianum.cat>
2024-04-30 16:17:56 +0200

    ok

--- UF3/validacions.js
+++ UF3/validacions.js
@@ -28,13 +28,50 @@ function displayError(input, message) {
 function validateName() {
   removeExistingError(nameInput);
   // Validació del nom...
-  if (username.length < 3) {
+  if (name.length < 3) {
     isValid = false;
-} else {
-    usernameError.textContent = '';
-}
+  } else {
+    usernameError.textContent = "";
+  }
   // Si no és vàlid...
   displayError(nameInput, "El nom no és vàlid");
 }
-
+function validEmail() {
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailPattern.test(email)) {
+    emailError.textContent = "Possa un correu valid";
+    isValid = false;
+  } else {
+    emailError.textContent = "";
+  }
+}
 // addEventListeners...
+registrationForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const registrationForm = document.getElementById("");
+  const nameInput = document.getElementById("name").value;
+  const surnameInput = document.getElementById("surname").value;
+  const emailInput = document.getElementById("email").value;
+  const ageInput = document.getElementById("age").value;
+  // const gender = characterItem.querySelector('strong:nth-of-type(3)').textContent.slice(8);
+  const genderInput = document.getElementById("gender").value;
+  const messageInput = document.getElementById("message").value;
+  const passwordInput = document.getElementById("password").value;
+  if (name.length < 3) {
+    usernameError.textContent =
+      "El nombre de usuario debe tener al menos 3 caracteres.";
+    isValid = false;
+  } else {
+    usernameError.textContent = "";
+  }
+
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailPattern.test(email)) {
+    emailError.textContent = "Possa un correu electronic";
+    isValid = false;
+  } else {
+    emailError.textContent = "";
+  }
+
+});
